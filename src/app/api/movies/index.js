1fb291db811a457f25c531adb727bdb267e36a96
kwongster <bryant.j.kwong@gmail.com>
2023-04-23 23:12:17 -0700

    route api created in nextjs folder

--- src/app/api/movies/index.js
+++ src/app/api/movies/index.js
@@ -1,1 +1,20 @@
-import { getMovies } from "./../../lib/mongo/movies";
+// import { getMovies } from "./../../lib/mongo/movies";
+
+// const handler = async (req, res) => {
+//   if (req.method === "GET") {
+//     try {
+//       const { movies, error } = await getMovies();
+//       if (error) throw new Error(error);
+//       return res.status(200).json({ movies });
+//     } catch (error) {
+//       return res.status(500).json({ error: error.message });
+//     }
+//   }
+//   res.setHeader("Allow", ["GET"]);
+//   res.status(425).end(`Method ${req.method} is no allowed`);
+// };
+// export default handler;
+
+export async function GET() {
+  return new Response("Hello, Next.js!");
+}
