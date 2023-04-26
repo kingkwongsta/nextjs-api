80932e085bad16842bc5c3b834094c59de53de27
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-04-26 16:59:08 -0700

    add image hosting to whitelist

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// const nextConfig = {
+//   experimental: {
+//     appDir: true,
+//   },
+// }
+
+// module.exports = nextConfig
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+        port: "",
+      },
+    ],
+  },
   experimental: {
     appDir: true,
   },
-}
-
-module.exports = nextConfig
+};
