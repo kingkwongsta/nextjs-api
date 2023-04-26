00bcfa920cc3f0749af5c0c434a86867e5336484
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-04-25 21:29:22 -0700

    async function to call data from db

--- src/app/components/button.jsx
+++ src/app/components/button.jsx
@@ -1,24 +1,33 @@
-"use client";
-import { GET } from "./../api/movies/route";
+// "use client";
+// import { GET } from "./../api/movies/route";
+import { getMovies } from "./../../../lib/mongo/movies";
 
-export default function Button() {
-  function handleClick() {
-    console.log("hi");
-  }
-  return (
-    <div>
-      <button className="border-2 p-2" onClick={handleClick}>
-        HELLO
-      </button>
-    </div>
-  );
+// export async function getStaticProps() {
+//   const message = await GET("message");
+//   console.log(message);
+//   return message;
+// }
+
+async function fetchMovies() {
+  const { movies } = await getMovies();
+  return movies;
 }
 
-export async function getServerSideProps() {
-  const data = await GET();
-  return {
-    props: {
-      data: data,
-    },
-  };
+export default async function Button({ message }) {
+  //   function handleClick() {
+  //     console.log("hi");
+  //   }
+  const movies = await fetchMovies();
+  return (
+    <div>
+      <button className="border-2 p-2">HELLO</button>
+      <div>
+        <ul>
+          {movies.map((movie) => (
+            <li key={movie._id}>{movie.title}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
