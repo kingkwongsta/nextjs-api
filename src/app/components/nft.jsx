4c6699b5726c7c8572eb32868ee162de6d4c1e8d
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-05-02 20:33:31 -0700

    create base of nft component

---
+++ src/app/components/nft.jsx
@@ -0,0 +1,12 @@
+"use client";
+import { useState, useEffect } from "react";
+
+// 4OaFk7STzmZ2D2ElyiKupVSbXyPqRwaJ
+
+export default function NFT() {
+  return (
+    <>
+      <div>Hello</div>
+    </>
+  );
+}
