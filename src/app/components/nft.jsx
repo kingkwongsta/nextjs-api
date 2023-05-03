de8a94e019fd6eacea2f6ce454662b73e298358e
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-05-02 20:39:31 -0700

    added API call to get data

--- src/app/components/nft.jsx
+++ src/app/components/nft.jsx
@@ -6,9 +6,21 @@ import { useState, useEffect } from "react";
 export default function NFT() {
   const [nftData, setNftData] = useState();
 
+  useEffect(() => {
+    getData();
+  }, []);
+
   function showState() {
     console.log(nftData);
   }
+
+  const getData = async () => {
+    const res = await fetch(
+      "https://api.dune.com/api/v1/query/2435066/results?api_key=4OaFk7STzmZ2D2ElyiKupVSbXyPqRwaJ"
+    );
+    const data = await res.json();
+    setNftData(data.result);
+  };
 
   return (
     <div className="m-10">
