914ac1b0f4c09a263f334397b3d69242b4360581
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-05-02 20:34:46 -0700

    add nft to page to test component

--- src/app/components/nft.jsx
+++ src/app/components/nft.jsx
@@ -4,9 +4,17 @@ import { useState, useEffect } from "react";
 // 4OaFk7STzmZ2D2ElyiKupVSbXyPqRwaJ
 
 export default function NFT() {
+  const [nftData, setNftData] = useState();
+
+  function showState() {
+    console.log(nftData);
+  }
+
   return (
-    <>
-      <div>Hello</div>
-    </>
+    <div className="m-10">
+      <button onClick={showState} className="border-2 p-3">
+        What is in State
+      </button>
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Movie from "./components/movie";
 import Restaurants from "./components/restaurants";
+import NFT from "./components/nft";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,8 +10,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <Restaurants />
-        <Movie />
+        <NFT />
+        {/* <Restaurants />
+        <Movie /> */}
       </div>
     </main>
   );
