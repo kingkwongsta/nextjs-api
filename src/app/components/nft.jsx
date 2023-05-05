d322564bd1ab6be68bd002d779fa2314a7afaa98
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-05-04 23:39:27 -0700

    connection to nft database

--- src/app/components/nft.jsx
+++ src/app/components/nft.jsx
@@ -2,28 +2,19 @@
 
 "use client";
 import { useState, useEffect } from "react";
-import importedData from "./../api/nft/nftData";
 
 // 4OaFk7STzmZ2D2ElyiKupVSbXyPqRwaJ
 
 export default function NFT() {
   const [nftData, setNftData] = useState();
-  const apiKey = "g8YVZgY4LmKtF2gW0Afip63lEZK7V9as";
-  const APIEndpoint = `https://api.dune.com/api/v1/query/2443176/results?api_key=${apiKey}`;
-  // const apiKey = process.env.DUNEAPI;
-  useEffect(() => {
-    getData();
-  }, []);
-  console.log(APIEndpoint);
+  useEffect(() => {}, []);
 
   function showState() {
     console.log(nftData);
   }
 
   const getData = async () => {
-    const res = await fetch(
-      "https://api.dune.com/api/v1/query/2443176/results?api_key=g8YVZgY4LmKtF2gW0Afip63lEZK7V9as"
-    );
+    const res = await fetch();
     const data = await res.json();
     setNftData(data.result);
   };
