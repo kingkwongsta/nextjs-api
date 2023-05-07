cbd60eeb0712532e999149cfe4c1689b9455675a
kwongster <bryant.j.kwong@gmail.com>
2023-05-06 22:53:37 -0700

    run fetchData on initial load with useEffect state

--- src/app/components/test.jsx
+++ src/app/components/test.jsx
@@ -1,13 +1,17 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Test() {
   const [queryData, setQueryData] = useState();
 
+  useEffect(() => {
+    fetchData();
+  }, []);
   const fetchData = async () => {
     const res = await fetch("/api/web3/arbitrum");
     const data = await res.json();
     setQueryData(data);
+    console.log(data);
   };
 
   return (
